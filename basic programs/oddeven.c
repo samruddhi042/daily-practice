1557338c3c721e520af0390916a366b19b1bf389
Samruddhi Chavan <samruddhic006@gmail.com>
2026-06-23 16:40:12 +0530

    if a number is odd or even code in c

--- basic programs/oddeven.c
+++ basic programs/oddeven.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int a; // Declare variable 'a'
+    int a;      //declare variable 'a'
 
-    printf("Enter an integer: "); // Proper message formatting
-    scanf("%d", &a); // No space between '&' and 'a'
+    printf("Enter an integer: ");
+    scanf("%d", &a);
 
     // Check if the number is even or odd
     if (a % 2 == 0) {
-        printf("%d is an even number.\n", a); // Corrected printf statement
+        printf("%d is an even number.\n", a); 
     } else {
         printf("%d is an odd number.\n", a);
     }
