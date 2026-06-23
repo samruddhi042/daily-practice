e3c3a9a4124547a338b20addadcdc3d67ed7906c
Samruddhi Chavan <samruddhic006@gmail.com>
2026-06-23 16:36:16 +0530

    sum of 1 tp N numbers using formula code, O(1)TC/SC

--- basic programs/sumof_1toN_usingformuula.cpp
+++ basic programs/sumof_1toN_usingformuula.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     int n;
-    cout<<"enter value of n : "; //time complexity O(1), space complexity O(1), much faster
+    cout<<"enter value of n : ";   //time complexity O(1), space complexity O(1), much faster
     cin>>n;
     int sum=0;
 
