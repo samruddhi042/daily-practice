34514543e4693adcd6781bbddd68ffa6c842bf04
Samruddhi Chavan <samruddhic006@gmail.com>
2026-06-28 20:34:04 +0530

    leap year check 2.0

--- basic programs/leap_year_check.cpp
+++ basic programs/leap_year_check.cpp
@@ -1,13 +1,24 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+bool (isleapyear(int year)){
+    if ((year%4==0&&year%100!=0) || year%400==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
 
 int main(){
     int year;
-    cout<<"Enter a year: ";
+    cout<<"Enter a year to check if its a leap year or not: ";
     cin>>year;
-    if ((year % 4==0 && year % 100!=0)||(year % 400==0))
-        cout<<" "<<year<<" is a Leap Year";
-    else
-        cout<<" "<<year<<" is not a Leap Year";
-    return 0;
+    if(isleapyear(year)){
+        cout<<"The year "<<year<<" is leap year";
+    }
+    else{
+        cout<<"The year "<<year<<" is not a leap year";
+    }
 }
