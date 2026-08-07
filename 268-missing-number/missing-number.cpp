251ef99a355618fb8ea74ca7505418d5a2f35481
Samruddhi Chavan <samruddhic006@gmail.com>
2026-08-08 01:27:08 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (39.43%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums){
         int range=nums.size();
-        int actualSum=(range*(range+1))/2;  
+        int actualSum=(range*(range+1))/2;  //(n*(n+1))/2
+        //actual sum when the missing number is present in array
         int currSum=0;
 
         for(int i=0; i<nums.size();i++){
