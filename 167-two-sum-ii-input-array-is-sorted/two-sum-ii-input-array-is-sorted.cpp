35d58e0c3bf7e668c33a29843620f1836b1805d5
Samruddhi Chavan <samruddhic006@gmail.com>
2026-08-08 01:48:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (65.57%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -6,8 +6,9 @@ public:
         while (start<end){
 
             int sum =numbers[start]+numbers[end];
+            //target found
             if (sum ==target){
-                return {start+1,end+1};
+                return {start+1,end+1}; //return 1-based indices
             }
             else if(sum>target){
                 end--;
