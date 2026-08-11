8ea67f8df0d0744c7e193a8c8e3e29b3294005dc
Samruddhi Chavan <samruddhic006@gmail.com>
2026-08-11 15:52:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (69.86%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.cpp
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -3,4 +3,4 @@ public:
     int bulbSwitch(int n) {
         return sqrt(n);
     }
-};
+};  //a bulb remains ON only if it is toggled an odd number of times. This happens only for bulb positions that are perfect squares. thus the problem reduces to counting the number of perfect squares less than or equal to n.
